7b2b7ff699c58a0d5d7d8481564499b064e5b279
Henrique <rickdbs.dev@hotmail.com>
2023-07-28 19:57:01 -0300

    Projeto de consulta do clima

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+const api = "1b6972a72ec6001b218bca360d2c2404"
+
+const cityInput = document.querySelector("#city-input")
+const searchBtn = document.querySelector("#search")
+
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const humidityElement = document.querySelector("#humidity span")
+const windElement = document.querySelector("#wind span")
+const weatherContainer = document.querySelector("#weather-data")
+
+
+const getWeatherData = async(city) =>{
+  const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}&lang=pt_br`
+
+  const res = await fetch(apiWeatherUrl)
+  const data = await res.json()
+
+  return data
+}
+
+const showWeatherData = async (city) => {
+
+  const data = await getWeatherData(city)
+
+  cityElement.innerText = data.name
+  tempElement.innerText = parseInt(data.main.temp)
+  descElement.innerText = data.weather[0].description
+  weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+  countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`)
+  humidityElement.innerText = `${data.main.humidity}%`
+  windElement.innerText = `${data.wind.speed}km/h`
+
+  weatherContainer.classList.remove('hide')
+}
+
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  const city = cityInput.value
+  
+  showWeatherData(city)
+})
+
+cityInput.addEventListener("keyup", (e) => {
+  if(e.code === "Enter"){
+    const city = e.target.value
+
+    showWeatherData(city)
+  }
+})
